d5f7610999ae10172665d486abeaa0fd004052ee
ocalvario <oswald.calvario@gmail.com>
2020-07-03 15:51:31 -0400

    fixed to login process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
     def home
       redirect_to login_path
+    end
+    
+    def log_in(user)
+      session[:user_id] = user.id
     end 
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,15 @@ class SessionsController < ApplicationController
         @user = User.new
     end 
 
+    def create
+        user = User.find_by(email: params[:user][:email]) 
+        if user && user.authenticate(password: params[:user][:password])
+            #logging in
+            log_in(user)
+            redirect_to user
+        else
+            render :new
+        end 
+    end 
+
 end
