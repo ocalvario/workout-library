b86e8cdb3846c8d0316224d808da020693d873b7
ocalvario <oswald.calvario@gmail.com>
2020-07-03 17:18:48 -0400

    gets signup to render show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,11 @@
 class UsersController < ApplicationController
     
     def new
-        if logged_in?
-            redirect_to user_path(current_user)
-        else
             @user = User.new
-        end
     end 
 
     def create
-        @user = User.create(
-            email: params[:email], 
-            password: params[:password]
-        )
+        @user = User.new(user_params)
         if @user.save
             log_in(@user)
             redirect_to @user
@@ -23,6 +16,12 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find(params[:id]) 
+    end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
     end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,20 @@ Rails.application.routes.draw do
     get '/signup', to: 'users#new'
     post '/signup', to: 'users#create'
 
+    #Logging In
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
+  
   resources :reviews
   resources :exercises
+  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root :to => "hw#hello_world"
 
-  #Logging In
-  
-    get '/login', to: 'sessions#new'
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
+
 end
