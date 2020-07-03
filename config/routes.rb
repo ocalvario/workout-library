5c5ceaed9b1b84557a217a96bfdd6c77d7027211
ocalvario <oswald.calvario@gmail.com>
2020-07-03 16:51:09 -0400

    fixes signup form initial issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
   
   #root_route
     root to: 'application#home'
+    
+
+  #Signing Up 
+    get '/signup', to: 'users#new'
+    post '/signup', to: 'users#create'
 
   resources :reviews
   resources :exercises
-  resources :users, excelpt: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root :to => "hw#hello_world"
-
-  #Signing Up
-  get '/signup', to: 'users#new'
 
   #Logging In
   
