ab95782778930a0b2d2c4b0160b4c4cd1d8aceff
ocalvario <oswald.calvario@gmail.com>
2020-07-03 13:25:04 -0400

    adds initial user routes signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :exercises
-  resources :users
+  resources :users, excelpt: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root :to => "hw#hello_world"
+  #root :to => "hw#hello_world"
+
+  #Signing Up
+  get '/signup', to: 'users#new'
+
+  #Logging In
+  
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
 end
