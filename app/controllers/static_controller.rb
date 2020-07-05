eaacb77df8acae78326e105b254ec88a76f374c5
ocalvario <oswald.calvario@gmail.com>
2020-07-04 22:27:12 -0400

    adds signup and login from up, checks if logged in

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,12 @@
 class StaticController < ApplicationController
 
     def home
-        render :home
+        @user = current_user
+        if logged_in?
+            redirect_to user_path(@user)
+        else    
+            render :home
+        end
     end
 
 end 
