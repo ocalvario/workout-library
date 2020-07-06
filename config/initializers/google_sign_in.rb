545b9ca430a74f8c7840ffb2949a475bfc8b63b3
ocalvario <oswald.calvario@gmail.com>
2020-07-06 11:53:02 -0400

    Makes information more tabular

---
+++ config/initializers/google_sign_in.rb
@@ -0,0 +1,4 @@
+Rails.application.configure do
+    config.google_sign_in.client_id     = ENV['691559643335-g8ns0mjj96ddt19k3f3d45pa57l6jb2a.apps.googleusercontent.com']
+    config.google_sign_in.client_secret = ENV['csxeYMoSMUUBzbYA9tcjvCSZ']
+  end
