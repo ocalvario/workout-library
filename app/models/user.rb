c4b112b9793963c93e5f783307f6a4ba5b9287c5
ocalvario <oswald.calvario@gmail.com>
2020-07-18 18:00:26 -0400

    Adds form page to different classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-    
     has_secure_password
-    
     has_many :reviews
     has_many :exercises, through: :reviews
-
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
 
