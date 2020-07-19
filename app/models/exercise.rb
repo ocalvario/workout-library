ab5f3fdb49471748158f61638b4452b256e92efb
ocalvario <oswald.calvario@gmail.com>
2020-07-19 10:56:16 -0400

    Minor formatting clean

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,8 +1,8 @@
 class Exercise < ApplicationRecord
     has_many :reviews
     belongs_to :user
-
     validates :name, uniqueness: true
     validates_presence_of :name, :description
-
 end
+
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,5 @@ class Review < ApplicationRecord
     belongs_to :user, counter_cache: true
     belongs_to :exercise, counter_cache: true
     has_many :workouts, through: :exercise 
-
     validates_presence_of :content
-
 end
